d544b5f247ea74364c91f37bb24d9556040f72f0
honjow <honjow311@gmail.com>
2023-08-29 05:41:32 +0800

    add install services

---
+++ scripts/install_services.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Check if the script is running with root or sudo privileges
+if [ "$EUID" -ne 0 ]; then
+    echo "This script must be run as root or with sudo."
+    exit 1
+fi
+
+cp ./system/* /etc/systemd/system/
+
+systemctl enable resume.service
+systemctl enable update_boot_config.service
+systemctl enable vim_config_update.service
